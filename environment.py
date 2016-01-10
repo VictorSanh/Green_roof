0da03195a69b080897753d69b50db4f121a3007c
Zakariya LOULOU <Zakariya LOULOU>
2016-01-10 20:32:52 +0100

    ca avance

--- environment.py
+++ environment.py
@@ -12,10 +12,11 @@ import csv
 import json
 import math
 
-     
+
 def detecte_IDstation():
     # Stock dans un dictionnaire les longitudes, latitues et les ID des 
     # stations correspondantes
+
     dico = {}
     with open('postesSynop.json') as file:
             doc=json.load(file)
@@ -25,19 +26,44 @@ def detecte_IDstation():
                 dico[element["Longitude"],element["Latitude"]] = element["ID"]
     return dico
 
-a=detecte_IDstation()
-for cle in a:
-    print(cle[0])
+b = detecte_IDstation()
+L = list(b.keys())
+
+class ValeurNonCommuniquee(Exception): 
+    def __init__(self):
+        super().__init__('La valeur demandée nest pas communiquée')
 
 def repere_station(long, lati):
     #Repère la station la plus proche du point ou a cliqué l'utilisateur pour 
     #y extraire les données
-    #c'est pas encore fini!!
-    diconaire = detecte_IDstation()
-    list_vector = []
-    for cle in diconaire:
-        list_vector.append(math.sqrt((cle[0] - long)**2 + (cle[1] - lati)**2))
 
+    diconaire = detecte_IDstation()
+    L = list(diconaire.keys())
+    long_station = float(L[0][0])
+    lati_station = float(L[0][1])
+    minimum = math.sqrt((long_station - long)**2 + (lati_station - lati)**2)
+    for cle in diconaire:
+        if math.sqrt((float(cle[0]) - long)**2 + (float(cle[1]) - lati)**2) <= minimum:
+            long_station = cle[0]
+            lati_station = cle[1]
+    return(long_station, lati_station)
+
+a = repere_station(54.520667,-15.887667)
+
+def get_temp(annee, mois, jour, heure, ID):
+    """ Donne la température en Kelvin à un instant donné """
+    s = 'synop.{}{}.csv.gz'.format(annee, mois)
+    with open(s) as csvfile:
+        doc = csv.reader(csvfile, delimiter = ';')
+        for row in doc:      
+            if row[1] == annee+mois+jour+heure and row[0] == ID:              
+                a = row[7]
+        if a == 'mq':
+            raise ValeurNonCommuniquee()
+    return a
+
+c = get_temp('2014', '12', '01', '060000', '07139')
+print(c)
 
 class Environment(object):
     """
@@ -52,19 +78,38 @@ class Environment(object):
         self.latitude = latitude
         self.date = date
 
-    def get_t(self, time):
-        """ Donne la température à un instant donné """
-        
-        return 0
+    def get_t(self, annee, mois, jour, heure, ID):
+        """ Donne la température en Kelvin à un instant donné """
+        s = 'synop.{}{}.csv.gz'.format(annee, mois)
+        with open(s) as csvfile:
+            doc = csv.reader(csvfile, delimiter = ';')
+            for row in doc:      
+                if row[1] == annee+mois+jour+heure and row[0] == ID:              
+                    a = row[7]
+            if a == 'mq':
+                raise ValeurNonCommuniquee()
+        return a
 
-    def irradiance(self, time):
-        """ Donne les radiations solaires (W/m^2) à un moment de la journée """
-        return 0
-
-    def get_pres(self, time):
+    def get_pres(self, annee, mois, jour, heure, ID):
         """ Donne la pression à un moment de la journée """
-        return 0
+        s = 'synop.{}{}.csv.gz'.format(annee, mois)    
+        with open(s) as csvfile:
+            doc = csv.reader(csvfile, delimiter = ';')     
+            for row in doc:      
+                if row[1] == annee+mois+jour+heure and row[0] == ID:              
+                    a = row[20]
+            if a == 'mq':
+                raise ValeurNonCommuniquee()
+        return a
         
-    def get_u(self,time):
+    def get_u(self, annee, mois, jour, heure, ID):
         """ Donne l'humidité ambiante à un instant donné """
-        return 0
+        s = 'synop.{}{}.csv.gz'.format(annee, mois)
+        with open(s) as csvfile:
+            doc = csv.reader(csvfile, delimiter = ';')
+            for row in doc:      
+                if row[1] == annee+mois+jour+heure and row[0] == ID:              
+                    a = row[9]
+            if a == 'mq':
+                raise ValeurNonCommuniquee()
+        return a
