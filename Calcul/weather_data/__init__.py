2c267a5ac5160fa599fb2651207a1a386704ca68
Dylan <dylan.dronnier@laposte.net>
2016-01-19 19:57:39 +0100

    :D

--- Calcul/weather_data/__init__.py
+++ Calcul/weather_data/__init__.py
@@ -1,5 +1,7 @@
 from os import path
 resources_dir = path.join(path.dirname(__file__), 'resources')
+code_fortran_dir = path.join(path.dirname(__file__), 'code_fortran')
+data_base_dir = path.join(path.dirname(__file__), 'data_base')
 
 from .donnees_json import *
 from .irradiance import *
