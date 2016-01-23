2983187f842d77fde0103d4ac44a7d7d9d8b3422
VictorSanh <s.rotciv@hotmail.fr>
2016-01-23 17:45:08 +0100

    Update confirmation.py

--- confirmation.py
+++ confirmation.py
@@ -1,98 +1,124 @@
 # -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 12 18:24:19 2016
 
-# Form implementation generated from reading ui file 'confirmation.ui'
+@author: srotc_000
+"""
+
+
+import urllib.parse
+import urllib.request
+from urllib.error import URLError, HTTPError
+import json 
+from unidecode import unidecode
+
+class ZeroResult(Exception):
+    def __init__(self,valeur):
+        self.valeur = valeur
+    def __imprim__(self):
+        return "{}".format(self.valeur)
+
+class ZeroResultFrance(Exception):
+    def __init__(self,valeur):
+        self.valeur = valeur
+    def __imprim__(self):
+        return "{}".format(self.valeur)
+
+#Requete html sur googleapis.com/maps sous la forme suivante :
+#https://maps.googleapis.com/maps/api/geocode/json?address=avenue+blaise+pascal+champs+sur+marnes&sensor=false
+#Ca renvoie un fichier json avec beaucoup de données sur l'adresse indiquée, dont notamment la lat et longitude.
+
+def geocode(addr):
+        """Renvoie un fichier json qui modélise la position indiquée par addr"""
+        
+        addr_formatee = addr.replace(' ','+')     
+        url = ("https://maps.googleapis.com/maps/api/geocode/json?address=%s&sensor=false" % addr_formatee);        
+        #Gestion des accents : é, à,...        
+        url = unidecode(url)
+        reponse = urllib.request.urlopen(url).read()
+        #Problème: reponse est de type bytes, et donc json.load() ne peut le lire.        
+        #On préfère donc le convertir avant la lecture sous json.        
+        data = reponse.decode('utf-8')        
+        return json.loads(data)
 #
-# Created: Tue Jan 19 16:47:32 2016
-#      by: PyQt4 UI code generator 4.10.4
+def trouve_en_france(fichier):
+    """Va checker si le champ 'status' du fichier json est bien OK i.e.
+    qu'une ville a été trouvée (même si c'est pas la bonne).
+    Si le champs 'status' vaut ZERO_RESULTS, c'est qu'il y a rien de trouvé
+    et qu'il faut revoir l'adresse entrée. On lève dans ce cas un exception."""
+    
+    if fichier['status']=="ZERO_RESULTS":
+        raise ZeroResult("Pas de résultat")
+    else:
+        liste_results=fichier['results']
+        position=-1
+        for i, element in enumerate(liste_results):
+            for ss_element in element['address_components']:
+                if ss_element['short_name']=="FR":
+                    position=i
+        if position==-1:
+            raise ZeroResultFrance("Pas de résultat en France")
+        else:
+            return position
+
+
+def addresse_formatee(fichier, position):
+        """Va renvoyer l'adresse formatée selon les criètes de Google Maps 
+        afin que l'utilisateur vérifie que ce soit bien la bonne adresse."""
+        
+        return fichier['results'][position]['formatted_address']
+        
+
+def lat_lng(fichier, position):
+        """Si l'adresse est validée par l'utilisateur comme son adresse 
+        (addresse_formatee), alors on renvoie réellement la latitude et 
+        la longitude sous la forme d'un dictionaire 
+        {'lat' : valeur_latitude, 'lng' : valeur_longitude}"""
+        
+        return fichier['results'][position]['geometry']['location']
+        
+        
+#def main(adresse_rentree):
+#    try:
+#        fichier = geocode(adresse_rentree)
+#        pos = trouve_en_france(fichier)
+#    except MonException as erreur:
+#        print(erreur)
+#        erreur.__imprim__()
+#    except HTTPError as erreur:
+#        print(erreur)
+#        erreur.__str__()
+#    except URLError as erreur:
+#        print(erreur)
+#        erreur.__str__()
+#    except:
+#        print("Something went wrong")        
+#    else:
+#        print(addresse_formatee(fichier, pos))
+#        latlng = lat_lng(fichier, pos)
+#        print (latlng['lat'],latlng['lng'])
+
 #
-# WARNING! All changes made in this file will be lost!
+#if __name__=="__main__":
+#    main()
+    
 
-import parametres_utilisateur
-from PyQt4 import QtCore, QtGui
+#Il faut donner de la robustesse au code et la gestion des erreurs
+#Par exemple, il y a URLError quand il n'y a pas de connexion internet :
+#<urlopen error [Errno 11001] getaddrinfo failed>
+#HTTPError quand l'adresse n'est pas trouvée ou
+#IndexError quand on veut accéder à un champ du fichier json alors que celui ci est vide.
 
-try:
-    _fromUtf8 = QtCore.QString.fromUtf8
-except AttributeError:
-    def _fromUtf8(s):
-        return s
 
-try:
-    _encoding = QtGui.QApplication.UnicodeUTF8
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig, _encoding)
-except AttributeError:
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig)
+#Autre possibilité alternative avec la libraire geopy.
 
-class Ui_Confirmation(object):
-    def setupUi(self, Confirmation, adresseGoogleFormated, latlng):
-        Confirmation.setObjectName(_fromUtf8("Confirmation"))
-        Confirmation.setEnabled(True)
-        Confirmation.resize(500, 300)
-        Confirmation.setMinimumSize(QtCore.QSize(500, 300))
-        Confirmation.setMaximumSize(QtCore.QSize(500, 300))
-        self.monBackground = QtGui.QLabel(Confirmation)
-        self.monBackground.setGeometry(QtCore.QRect(0, 0, 500, 300))
-        self.monBackground.setMinimumSize(QtCore.QSize(500, 300))
-        self.monBackground.setMaximumSize(QtCore.QSize(500, 300))
-        self.monBackground.setText(_fromUtf8(""))
-        self.monBackground.setPixmap(QtGui.QPixmap(_fromUtf8("point_d_interrogation.png")))
-        self.monBackground.setScaledContents(True)
-        self.monBackground.setWordWrap(False)
-        self.monBackground.setObjectName(_fromUtf8("monBackground"))
-        
-        self.bonneAdresse = QtGui.QPushButton(Confirmation)
-        self.bonneAdresse.setGeometry(QtCore.QRect(10, 240, 211, 51))
-        self.bonneAdresse.setStyleSheet(_fromUtf8("background-color:rgb(51,255,51);\n"
-"color: rgb(255, 255, 255);\n"
-"font: 75 15pt \"Calibri\";\n"
-"border-top-color: rgb(121, 255, 229);\n"
-"border-radius: 10px;"))
-        self.bonneAdresse.setObjectName(_fromUtf8("bonneAdresse"))
-        self.bonneAdresse.clicked.connect(Confirmation.close)
-        self.bonneAdresse.clicked.connect(lambda: self.paramUtilisateur(latlng))
-        
-        self.preciserAdresse = QtGui.QPushButton(Confirmation)
-        self.preciserAdresse.setGeometry(QtCore.QRect(290, 240, 201, 51))
-        self.preciserAdresse.setStyleSheet(_fromUtf8("background-color:rgb(51,255,51);\n"
-"color: rgb(255, 255, 255);\n"
-"font: 75 15pt \"Calibri\";\n"
-"border-top-color: rgb(121, 255, 229);\n"
-"border-radius: 10px;"))
-        self.preciserAdresse.setObjectName(_fromUtf8("preciserAdresse"))
-        self.preciserAdresse.clicked.connect(Confirmation.close)        
-        
-        self.printAdresse = QtGui.QTextEdit(Confirmation)
-        self.printAdresse.setEnabled(False)
-        self.printAdresse.setGeometry(QtCore.QRect(50, 20, 400, 200))
-        self.printAdresse.setStyleSheet(_fromUtf8("background-color:rgba(51,255,51,1);\n"
-"color: rgb(255, 255, 255);\n"
-"font: 75 20pt \"Calibri\";\n"
-""))
-        self.printAdresse.setReadOnly(True)
-        self.printAdresse.setObjectName(_fromUtf8("printAdresse"))
+#from geopy.geocoders import Nominatim
+#geolocator = Nominatim()
+#location = geolocator.geocode("aveenue blaise pascal, champs sur marne")
+#print(location.address)
+#print((location.latitude, location.longitude))
 
-        self.retranslateUi(Confirmation, adresseGoogleFormated)
-        QtCore.QMetaObject.connectSlotsByName(Confirmation)
+#COMMENTAIRE SUR CE DERNIER CODE
 
-    def retranslateUi(self, Confirmation, adresseGoogleFormated):
-        Confirmation.setWindowTitle(_translate("Confirmation", "Dialog", None))
-        self.bonneAdresse.setText(_translate("Confirmation", "C\'est la bonne adresse !", None))
-        self.preciserAdresse.setText(_translate("Confirmation", "Préciser mon adresse...", None))
-        self.printAdresse.setText(_translate("Confirmation", adresseGoogleFormated, None))
-        
-    def paramUtilisateur(self, latlng):        
-        paramWindow = QtGui.QDialog()
-        ui = parametres_utilisateur.Ui_Param()
-        ui.setupUi(paramWindow, latlng)
-        paramWindow.exec_() 
-
-#if __name__ == "__main__":
-#    import sys
-#    app = QtGui.QApplication(sys.argv)
-#    Confirmation = QtGui.QDialog()
-#    ui = Ui_Confirmation()
-#    ui.setupUi(Confirmation,"Ceci est un test.")
-#    Confirmation.show()
-#    sys.exit(app.exec_())
+#Bien, renvoie rapidement MAIS ne gère pas vraiment les fautes d'orthographe.
 
