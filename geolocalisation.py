a7c37b3d26d852c64b903722a611a59217abff07
VictorSanh <s.rotciv@hotmail.fr>
2016-02-02 14:53:29 +0100

    Update geolocalisation.py

--- geolocalisation.py
+++ geolocalisation.py
@@ -1,10 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Jan 12 18:24:19 2016
-
-@author: srotc_000
-"""
-
 
 import urllib.parse
 import urllib.request
@@ -24,12 +18,12 @@ class ZeroResultFrance(Exception):
     def __imprim__(self):
         return "{}".format(self.valeur)
 
-#Requete html sur googleapis.com/maps sous la forme suivante :
-#https://maps.googleapis.com/maps/api/geocode/json?address=avenue+blaise+pascal+champs+sur+marnes&sensor=false
-#Ca renvoie un fichier json avec beaucoup de données sur l'adresse indiquée, dont notamment la lat et longitude.
-
 def geocode(addr):
         """Renvoie un fichier json qui modélise la position indiquée par addr"""
+        
+        #Requete html sur googleapis.com/maps sous la forme suivante :
+        #https://maps.googleapis.com/maps/api/geocode/json?address=avenue+blaise+pascal+champs+sur+marnes&sensor=false
+        #Ca renvoie un fichier json avec beaucoup de données sur l'adresse indiquée, dont notamment la lat et longitude.
         
         addr_formatee = addr.replace(' ','+')     
         url = ("https://maps.googleapis.com/maps/api/geocode/json?address=%s&sensor=false" % addr_formatee);        
@@ -76,49 +70,3 @@ def lat_lng(fichier, position):
         {'lat' : valeur_latitude, 'lng' : valeur_longitude}"""
         
         return fichier['results'][position]['geometry']['location']
-        
-        
-#def main(adresse_rentree):
-#    try:
-#        fichier = geocode(adresse_rentree)
-#        pos = trouve_en_france(fichier)
-#    except MonException as erreur:
-#        print(erreur)
-#        erreur.__imprim__()
-#    except HTTPError as erreur:
-#        print(erreur)
-#        erreur.__str__()
-#    except URLError as erreur:
-#        print(erreur)
-#        erreur.__str__()
-#    except:
-#        print("Something went wrong")        
-#    else:
-#        print(addresse_formatee(fichier, pos))
-#        latlng = lat_lng(fichier, pos)
-#        print (latlng['lat'],latlng['lng'])
-
-#
-#if __name__=="__main__":
-#    main()
-    
-
-#Il faut donner de la robustesse au code et la gestion des erreurs
-#Par exemple, il y a URLError quand il n'y a pas de connexion internet :
-#<urlopen error [Errno 11001] getaddrinfo failed>
-#HTTPError quand l'adresse n'est pas trouvée ou
-#IndexError quand on veut accéder à un champ du fichier json alors que celui ci est vide.
-
-
-#Autre possibilité alternative avec la libraire geopy.
-
-#from geopy.geocoders import Nominatim
-#geolocator = Nominatim()
-#location = geolocator.geocode("aveenue blaise pascal, champs sur marne")
-#print(location.address)
-#print((location.latitude, location.longitude))
-
-#COMMENTAIRE SUR CE DERNIER CODE
-
-#Bien, renvoie rapidement MAIS ne gère pas vraiment les fautes d'orthographe.
-
